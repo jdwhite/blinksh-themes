063daf87709b4eb19a7b3bd3671207cb462f97f2
Jason White <jdwhite@menelos.com>
2020-03-30 19:04:08 -0500

    Make better use of for loops; minor optimizations.

--- colors.sh
+++ colors.sh
@@ -5,17 +5,16 @@
 #
 # Jason White <jdwhite@menelos.com>
 #
-echo -n ' '
-for colheader in ' ' ' ' 4{0..7}m; do
-	printf " \033[m %3s " $colheader
+IFS=$'\n'
+for colhdr in '    ' ' ' 4{0..7}m; do
+	printf " \033[m %3s " $colhdr
 done
+unset IFS
 echo
-for fg in ' ' 1 {,1\;}31 {,1\;}32 {,1\;}33 {,1\;}34 {,1\;}35 {,1\;}36 {,1\;}37 ; do
+for fg in '' 1 {,1\;}31 {,1\;}32 {,1\;}33 {,1\;}34 {,1\;}35 {,1\;}36 {,1\;}37 ; do
 	printf "\033[m %5s \033[m" "${fg}m"
-	if [ "$fg" = " " ]; then fg=''; fi
-	printf " \033[${fg}m %3s \033[m" "gYw"
-	for bg in {40..47}; do
-		printf " \033[${fg};${bg}m %3s \033[m" "gYw"
+	for bg in '' \;{40..47}; do
+		printf " \033[${fg}${bg}m %3s \033[m" "gYw"
 	done
 	echo
 done
